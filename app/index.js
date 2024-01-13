c14ab4df532ad52a30a6e42d487321d1085c18e6
Norbert Kovács <112707533+MrBeard89@users.noreply.github.com>
2024-01-13 12:23:10 +0100

    native app init

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import { View, SafeAreaView, ScrollView } from 'react-native'
+import { useState } from 'react'
+import { Stack, useRouter } from 'expo-router'
+
+const Home = () => {
+  return <View></View>
+}
+
+export default Home
