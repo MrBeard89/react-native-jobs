ef2f9d4c98f8bd69a6e3a9585eaa951327e780ce
Norbert Kovács <112707533+MrBeard89@users.noreply.github.com>
2024-01-16 11:55:16 +0100

    Finished

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -5,7 +5,7 @@ import { FlatList, Image, Text, TextInput, TouchableOpacity, View } from 'react-
 import { icons, SIZES } from '../../../constants'
 import styles from './welcome.style'
 
-const Welcome = () => {
+const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
   const router = useRouter()
 
   const jobTypes = ['Full-Time', 'Part-Time', 'Contractor']
@@ -21,13 +21,13 @@ const Welcome = () => {
         <View style={styles.searchWrapper}>
           <TextInput
             style={styles.searchInput}
-            value=''
-            onChange={() => {}}
+            value={searchTerm}
+            onChangeText={(text) => setSearchTerm(text)}
             placeholder='What are you looking for?'
           />
         </View>
 
-        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+        <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
           <Image source={icons.search} resizeMode='contain' style={styles.searchBtnImage} />
         </TouchableOpacity>
       </View>
